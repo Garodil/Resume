5a14f6b1b68b34d7827a2d0d00bb7327322d3e9d
Garodil <37829429+Garodil@users.noreply.github.com>
2023-06-09 02:14:50 +0500

    ClassLibrary

--- Resume/Form1.cs
+++ Resume/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static ClassLibrary2.Library;
 
 namespace Resume
 {
@@ -23,40 +24,19 @@ namespace Resume
             if (String.IsNullOrEmpty(textBox1.Text) | String.IsNullOrEmpty(textBox2.Text) | String.IsNullOrEmpty(textBox3.Text))
             {
                 label2.Text = "Введите числа в поля";
-
+                
             }
-            else Check();
-
-        }
-
-        private void Check()
-        {
-            string astr = textBox1.Text;
-            string bstr = textBox2.Text;
-            string cstr = textBox3.Text;
-            bool resulta = double.TryParse(astr, out double a);
-            bool resultb = double.TryParse(bstr, out double b);
-            bool resultc = double.TryParse(cstr, out double c);
-
-            if (resulta && resultb && resultc)
+            else
             {
-                Result(a, b, c);
+                string astr = textBox1.Text;
+                string bstr = textBox2.Text;
+                string cstr = textBox3.Text;
+                label2.Text = TriangleSolver(astr, bstr, cstr);
+                
             }
-            else label2.Text = "Проверье числа в полях, возможно вы разделили десятичные знаки точкой, а не запятой, либо в ваши числа просочились буквы";
-        }
 
-        private void Result(double a, double b, double c)
-        {
 
-            double p = (a + b + c) / 2;
 
-            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-
-            if (Double.IsNaN(S))
-            {
-                label2.Text = "Такого треугольника нет";
-            }
-            else label2.Text = S.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -65,28 +45,11 @@ namespace Resume
             {
                 label4.Text = "Введите число в поле";
             }
-            else Check2();
+            else
+            {
+                label4.Text = RoundSolver(textBox4.Text);
+            }
         }
 
-        private void Check2()
-        {
-            string rstr = textBox4.Text;
-            bool resultr = double.TryParse(rstr, out double r);
-            if (resultr)
-            {
-                Result2(r);
-            }
-            else label4.Text = "Проверье число в поле, возможно вы разделили десятичные знаки точкой, а не запятой, либо в ваше число просочились буквы";
-        }
-
-        private void Result2(double r)
-        {
-            double S = Math.PI * Math.Pow(r, 2);
-            if (Double.IsNaN(S))
-            {
-                label4.Text = "Такого окружности нет";
-            }
-            else label4.Text = S.ToString();
-        }
     }
 }
